a3497fc22a07aed94dfd14ab25a45a263c0a7935
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2026-01-20 21:12:46 +0530

    Add SumCalculator component for summing input values

---
+++ src/components/SumCalculator.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function SumCalculator(){
+  const [input, setInput] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  function inputChanged() {
+    setSum((p) => p + Number(input));
+  }
+
+  useEffect(() => {
+    inputChanged();
+  }, [input]);
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <p>{sum}</p>
+    </div>
+  )
+  
+}
